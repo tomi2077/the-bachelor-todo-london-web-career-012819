9bc5807ce602a32b82c65486d7ff293dd0a47ceb
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-03 12:54:26 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -47,7 +47,7 @@ def get_average_age_for_season(data, season)
     contestant["age"].to_i
   end
 
-  summed_ages = contestant_ages.reduce(0) {|sum, age| sum += age}
+  summed_ages = ages.reduce(0) {|sum, age| sum += age}
   contestant_count =  contestant_ages.count.to_f
   (summed_ages/contestant_count).round
 
