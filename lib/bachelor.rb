26d4befb4c00f5a36a03984e84d334bad2835b07
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-03 12:16:42 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -33,7 +33,9 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |sea, info|
+    info.each do |k,v|
+      return k.values
 end
 
 def get_average_age_for_season(data, season)
